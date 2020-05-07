642b14dda8dc4c189143fe21c3c1a01f4893c554
roberto <betosimo@gmail.com>
2020-05-07 15:25:30 -0300

    first commit erro connection not found

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+
+
+
+  public async execute({ title, value, type, category }:
+    Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (!["income", "outcome"].includes(type)) {
+      throw new AppError("type invalid");
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError("you dont have enough balance");
+
+
+    }
+
+
+    const transaction = transactionsRepository.create({
+        title,
+        value,
+        type
+      });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
